f5a6a7831b0934f8c4cfa4d9be95d4985ea057f3
jke <kalebadagbe@gmail.com>
2026-09-25 18:05:15 +0100

    Simplify Flask app initialization
    Removed custom template folder setting from Flask app initialization.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from config import Config
 from extensions import db, migrate, login_manager, csrf
 
 def create_app():
-    app = Flask(__name__, template_folder='public')
+    app = Flask(__name__)
     app.config.from_object(Config)
 
     # Init extensions
